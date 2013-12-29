0fbf3973adaae4c861c16f5f34f60eb3a5d45c01
Mitar <mitar.git@tnode.com>
2013-12-29 09:58:42 +0100

    Version which with current version of django-overextends does not work.
    "base.html" theme from the "theme" app is skipped and its content is not
    rendered.

---
+++ overextends_test/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'overextends_test.views.home', name='home'),
+    # url(r'^overextends_test/', include('overextends_test.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'page.html'}),
+)
